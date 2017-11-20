192f070b83d2763b0723cc81e03d181c93f5a799
Vamsi <madasuvamsi@gmail.com>
2017-11-20 15:16:25 -0600

    Update CopyFile.py

--- CopyFile.py
+++ CopyFile.py
@@ -4,4 +4,3 @@ src_file="C:\\Users\\vamsi\\Desktop\\Src\\SalesData.csv";
 dst_file="C:\\Users\\vamsi\\Desktop\\Dst\\SalesData.csv";
 copyfile(src_file,dst_file)
 print("The File is Copied")
-# This is test
